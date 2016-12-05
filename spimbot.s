36836093f2b167e6861ca91157564548924d6634
Shenc0411 <SC@wirelessprv-10-193-254-85.near.illinois.edu>
2016-12-05 14:19:37 -0600

    1

--- spimbot.s
+++ spimbot.s
@@ -49,9 +49,534 @@ REQUEST_PUZZLE_INT_MASK = 0x800
 
 .data
 # data things go here
+.align 2
+TILE_DATA: .space 1600
+puzzle_data: .space 4096
+solution: .space 680
+three:	.float	3.0
+five:	.float	5.0
+PI:	.float	3.141592
+F180:	.float  180.0
 
 .text
 main:
 	# go wild
 	# the world is your oyster :)
-	j	main
+    li   $t0, BONK_MASK
+    or   $t0, $t0, TIMER_MASK
+    or   $t0, $t0, ON_FIRE_MASK
+    or   $t0, $t0, MAX_GROWTH_INT_MASK
+    or   $t0, $t0, REQUEST_PUZZLE_INT_MASK
+    or   $t0, $t0, 1
+    mtc0 $t0, $12
+
+    la   $t0, TILE_DATA
+    sw   $t0, TILE_SCAN
+
+    lw   $t0, GET_NUM_WATER_DROPS
+
+    li   $t0, 0
+    sw   $t0, SET_RESOURCE_TYPE
+    la   $t0, puzzle_data
+    sw   $t0, REQUEST_PUZZLE
+    la   $a0, solution
+    la   $a1, puzzle_data
+    jal  recursive_backtracking
+    la   $t0, solution
+    sw   $t0, SUBMIT_SOLUTION
+
+    lw   $t0, GET_NUM_WATER_DROPS
+
+main_loop:
+    lw   $t0, GET_NUM_SEEDS
+    beq  $t0, $0, request_seed
+    sw   $0, SEED_TILE
+    li   $t0, 1
+    sw   $t0, WATER_TILE
+    lw   $t0, GET_NUM_WATER_DROPS
+request_seed:
+    j    main_loop
+
+	j	 main
+
+# -----------------------------------------------------------------------
+# sb_arctan - computes the arctangent of y / x
+# $a0 - x
+# $a1 - y
+# returns the arctangent
+# -----------------------------------------------------------------------
+.globl sb_arctan
+sb_arctan:
+	li	$v0, 0		# angle = 0;
+
+	abs	$t0, $a0	# get absolute values
+	abs	$t1, $a1
+	ble	$t1, $t0, no_TURN_90	  
+
+	## if (abs(y) > abs(x)) { rotate 90 degrees }
+	move	$t0, $a1	# int temp = y;
+	neg	$a1, $a0	# y = -x;      
+	move	$a0, $t0	# x = temp;    
+	li	$v0, 90		# angle = 90;  
+
+no_TURN_90:
+	bgez	$a0, pos_x 	# skip if (x >= 0)
+
+	## if (x < 0) 
+	add	$v0, $v0, 180	# angle += 180;
+
+pos_x:
+	mtc1	$a0, $f0
+	mtc1	$a1, $f1
+	cvt.s.w $f0, $f0	# convert from ints to floats
+	cvt.s.w $f1, $f1
+	
+	div.s	$f0, $f1, $f0	# float v = (float) y / (float) x;
+
+	mul.s	$f1, $f0, $f0	# v^^2
+	mul.s	$f2, $f1, $f0	# v^^3
+	l.s	$f3, three	# load 5.0
+	div.s 	$f3, $f2, $f3	# v^^3/3
+	sub.s	$f6, $f0, $f3	# v - v^^3/3
+
+	mul.s	$f4, $f1, $f2	# v^^5
+	l.s	$f5, five	# load 3.0
+	div.s 	$f5, $f4, $f5	# v^^5/5
+	add.s	$f6, $f6, $f5	# value = v - v^^3/3 + v^^5/5
+
+	l.s	$f8, PI		# load PI
+	div.s	$f6, $f6, $f8	# value / PI
+	l.s	$f7, F180	# load 180.0
+	mul.s	$f6, $f6, $f7	# 180.0 * value / PI
+
+	cvt.w.s $f6, $f6	# convert "delta" back to integer
+	mfc1	$t0, $f6
+	add	$v0, $v0, $t0	# angle += delta
+
+	jr 	$ra
+
+#puzzle functions
+get_domain_for_cell:
+    # save registers    
+    sub $sp, $sp, 36
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
+    sw $s3, 16($sp)
+    sw $s4, 20($sp)
+    sw $s5, 24($sp)
+    sw $s6, 28($sp)
+    sw $s7, 32($sp)
+
+    li $t0, 0 # valid_domain
+    lw $t1, 4($a1) # puzzle->grid (t1 free)
+    sll $t2, $a0, 3 # position*8 (actual offset) (t2 free)
+    add $t3, $t1, $t2 # &puzzle->grid[position]
+    lw  $t4, 4($t3) # &puzzle->grid[position].cage
+    lw  $t5, 0($t4) # puzzle->grid[posiition].cage->operation
+
+    lw $t2, 4($t4) # puzzle->grid[position].cage->target
+
+    move $s0, $t2   # remain_target = $s0  *!*!
+    lw $s1, 8($t4) # remain_cell = $s1 = puzzle->grid[position].cage->num_cell
+    lw $s2, 0($t3) # domain_union = $s2 = puzzle->grid[position].domain
+    move $s3, $t4 # puzzle->grid[position].cage
+    li $s4, 0   # i = 0
+    move $s5, $t1 # $s5 = puzzle->grid
+    move $s6, $a0 # $s6 = position
+    # move $s7, $s2 # $s7 = puzzle->grid[position].domain
+
+    bne $t5, 0, gdfc_check_else_if
+
+    li $t1, 1
+    sub $t2, $t2, $t1 # (puzzle->grid[position].cage->target-1)
+    sll $v0, $t1, $t2 # valid_domain = 0x1 << (prev line comment)
+    j gdfc_end # somewhere!!!!!!!!
+
+gdfc_check_else_if:
+    bne $t5, '+', gdfc_check_else
+
+gdfc_else_if_loop:
+    lw $t5, 8($s3) # puzzle->grid[position].cage->num_cell
+    bge $s4, $t5, gdfc_for_end # branch if i >= puzzle->grid[position].cage->num_cell
+    sll $t1, $s4, 2 # i*4
+    lw $t6, 12($s3) # puzzle->grid[position].cage->positions
+    add $t1, $t6, $t1 # &puzzle->grid[position].cage->positions[i]
+    lw $t1, 0($t1) # pos = puzzle->grid[position].cage->positions[i]
+    add $s4, $s4, 1 # i++
+
+    sll $t2, $t1, 3 # pos * 8
+    add $s7, $s5, $t2 # &puzzle->grid[pos]
+    lw  $s7, 0($s7) # puzzle->grid[pos].domain
+
+    beq $t1, $s6 gdfc_else_if_else # branch if pos == position
+
+    
+
+    move $a0, $s7 # $a0 = puzzle->grid[pos].domain
+    jal is_single_value_domain
+    bne $v0, 1 gdfc_else_if_else # branch if !is_single_value_domain()
+    move $a0, $s7
+    jal convert_highest_bit_to_int
+    sub $s0, $s0, $v0 # remain_target -= convert_highest_bit_to_int
+    addi $s1, $s1, -1 # remain_cell -= 1
+    j gdfc_else_if_loop
+gdfc_else_if_else:
+    or $s2, $s2, $s7 # domain_union |= puzzle->grid[pos].domain
+    j gdfc_else_if_loop
+
+gdfc_for_end:
+    move $a0, $s0
+    move $a1, $s1
+    move $a2, $s2
+    jal get_domain_for_addition # $v0 = valid_domain = get_domain_for_addition()
+    j gdfc_end
+
+gdfc_check_else:
+    lw $t3, 12($s3) # puzzle->grid[position].cage->positions
+    lw $t0, 0($t3) # puzzle->grid[position].cage->positions[0]
+    lw $t1, 4($t3) # puzzle->grid[position].cage->positions[1]
+    xor $t0, $t0, $t1
+    xor $t0, $t0, $s6 # other_pos = $t0 = $t0 ^ position
+    lw $a0, 4($s3) # puzzle->grid[position].cage->target
+
+    sll $t2, $s6, 3 # position * 8
+    add $a1, $s5, $t2 # &puzzle->grid[position]
+    lw  $a1, 0($a1) # puzzle->grid[position].domain
+    # move $a1, $s7 
+
+    sll $t1, $t0, 3 # other_pos*8 (actual offset)
+    add $t3, $s5, $t1 # &puzzle->grid[other_pos]
+    lw $a2, 0($t3)  # puzzle->grid[other_pos].domian
+
+    jal get_domain_for_subtraction # $v0 = valid_domain = get_domain_for_subtraction()
+    # j gdfc_end
+gdfc_end:
+# restore registers
+    
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    lw $s2, 12($sp)
+    lw $s3, 16($sp)
+    lw $s4, 20($sp)
+    lw $s5, 24($sp)
+    lw $s6, 28($sp)
+    lw $s7, 32($sp)
+    add $sp, $sp, 36    
+    jr $ra
+
+forward_checking:
+  sub   $sp, $sp, 24
+  sw    $ra, 0($sp)
+  sw    $a0, 4($sp)
+  sw    $a1, 8($sp)
+  sw    $s0, 12($sp)
+  sw    $s1, 16($sp)
+  sw    $s2, 20($sp)
+  lw    $t0, 0($a1)     # size
+  li    $t1, 0          # col = 0
+fc_for_col:
+  bge   $t1, $t0, fc_end_for_col  # col < size
+  div   $a0, $t0
+  mfhi  $t2             # position % size
+  mflo  $t3             # position / size
+  beq   $t1, $t2, fc_for_col_continue    # if (col != position % size)
+  mul   $t4, $t3, $t0
+  add   $t4, $t4, $t1   # position / size * size + col
+  mul   $t4, $t4, 8
+  lw    $t5, 4($a1) # puzzle->grid
+  add   $t4, $t4, $t5   # &puzzle->grid[position / size * size + col].domain
+  mul   $t2, $a0, 8   # position * 8
+  add   $t2, $t5, $t2 # puzzle->grid[position]
+  lw    $t2, 0($t2) # puzzle -> grid[position].domain
+  not   $t2, $t2        # ~puzzle->grid[position].domain
+  lw    $t3, 0($t4) #
+  and   $t3, $t3, $t2
+  sw    $t3, 0($t4)
+  beq   $t3, $0, fc_return_zero # if (!puzzle->grid[position / size * size + col].domain)
+fc_for_col_continue:
+  add   $t1, $t1, 1     # col++
+  j     fc_for_col
+fc_end_for_col:
+  li    $t1, 0          # row = 0
+fc_for_row:
+  bge   $t1, $t0, fc_end_for_row  # row < size
+  div   $a0, $t0
+  mflo  $t2             # position / size
+  mfhi  $t3             # position % size
+  beq   $t1, $t2, fc_for_row_continue
+  lw    $t2, 4($a1)     # puzzle->grid
+  mul   $t4, $t1, $t0
+  add   $t4, $t4, $t3
+  mul   $t4, $t4, 8
+  add   $t4, $t2, $t4   # &puzzle->grid[row * size + position % size]
+  lw    $t6, 0($t4)
+  mul   $t5, $a0, 8
+  add   $t5, $t2, $t5
+  lw    $t5, 0($t5)     # puzzle->grid[position].domain
+  not   $t5, $t5
+  and   $t5, $t6, $t5
+  sw    $t5, 0($t4)
+  beq   $t5, $0, fc_return_zero
+fc_for_row_continue:
+  add   $t1, $t1, 1     # row++
+  j     fc_for_row
+fc_end_for_row:
+
+  li    $s0, 0          # i = 0
+fc_for_i:
+  lw    $t2, 4($a1)
+  mul   $t3, $a0, 8
+  add   $t2, $t2, $t3
+  lw    $t2, 4($t2)     # &puzzle->grid[position].cage
+  lw    $t3, 8($t2)     # puzzle->grid[position].cage->num_cell
+  bge   $s0, $t3, fc_return_one
+  lw    $t3, 12($t2)    # puzzle->grid[position].cage->positions
+  mul   $s1, $s0, 4
+  add   $t3, $t3, $s1
+  lw    $t3, 0($t3)     # pos
+  lw    $s1, 4($a1)
+  mul   $s2, $t3, 8
+  add   $s2, $s1, $s2   # &puzzle->grid[pos].domain
+  lw    $s1, 0($s2)
+  move  $a0, $t3
+  jal get_domain_for_cell
+  lw    $a0, 4($sp)
+  lw    $a1, 8($sp)
+  and   $s1, $s1, $v0
+  sw    $s1, 0($s2)     # puzzle->grid[pos].domain &= get_domain_for_cell(pos, puzzle)
+  beq   $s1, $0, fc_return_zero
+fc_for_i_continue:
+  add   $s0, $s0, 1     # i++
+  j     fc_for_i
+fc_return_one:
+  li    $v0, 1
+  j     fc_return
+fc_return_zero:
+  li    $v0, 0
+fc_return:
+  lw    $ra, 0($sp)
+  lw    $a0, 4($sp)
+  lw    $a1, 8($sp)
+  lw    $s0, 12($sp)
+  lw    $s1, 16($sp)
+  lw    $s2, 20($sp)
+  add   $sp, $sp, 24
+  jr    $ra
+
+ recursive_backtracking:
+  sub   $sp, $sp, 680
+  sw    $ra, 0($sp)
+  sw    $a0, 4($sp)     # solution
+  sw    $a1, 8($sp)     # puzzle
+  sw    $s0, 12($sp)    # position
+  sw    $s1, 16($sp)    # val
+  sw    $s2, 20($sp)    # 0x1 << (val - 1)
+                        # sizeof(Puzzle) = 8
+                        # sizeof(Cell [81]) = 648
+
+  jal   is_complete
+  bne   $v0, $0, recursive_backtracking_return_one
+  lw    $a0, 4($sp)     # solution
+  lw    $a1, 8($sp)     # puzzle
+  jal   get_unassigned_position
+  move  $s0, $v0        # position
+  li    $s1, 1          # val = 1
+recursive_backtracking_for_loop:
+  lw    $a0, 4($sp)     # solution
+  lw    $a1, 8($sp)     # puzzle
+  lw    $t0, 0($a1)     # puzzle->size
+  add   $t1, $t0, 1     # puzzle->size + 1
+  bge   $s1, $t1, recursive_backtracking_return_zero  # val < puzzle->size + 1
+  lw    $t1, 4($a1)     # puzzle->grid
+  mul   $t4, $s0, 8     # sizeof(Cell) = 8
+  add   $t1, $t1, $t4   # &puzzle->grid[position]
+  lw    $t1, 0($t1)     # puzzle->grid[position].domain
+  sub   $t4, $s1, 1     # val - 1
+  li    $t5, 1
+  sll   $s2, $t5, $t4   # 0x1 << (val - 1)
+  and   $t1, $t1, $s2   # puzzle->grid[position].domain & (0x1 << (val - 1))
+  beq   $t1, $0, recursive_backtracking_for_loop_continue # if (domain & (0x1 << (val - 1)))
+  mul   $t0, $s0, 4     # position * 4
+  add   $t0, $t0, $a0
+  add   $t0, $t0, 4     # &solution->assignment[position]
+  sw    $s1, 0($t0)     # solution->assignment[position] = val
+  lw    $t0, 0($a0)     # solution->size
+  add   $t0, $t0, 1
+  sw    $t0, 0($a0)     # solution->size++
+  add   $t0, $sp, 32    # &grid_copy
+  sw    $t0, 28($sp)    # puzzle_copy.grid = grid_copy !!!
+  move  $a0, $a1        # &puzzle
+  add   $a1, $sp, 24    # &puzzle_copy
+  jal   clone           # clone(puzzle, &puzzle_copy)
+  mul   $t0, $s0, 8     # !!! grid size 8
+  lw    $t1, 28($sp)
+  
+  add   $t1, $t1, $t0   # &puzzle_copy.grid[position]
+  sw    $s2, 0($t1)     # puzzle_copy.grid[position].domain = 0x1 << (val - 1);
+  move  $a0, $s0
+  add   $a1, $sp, 24
+  jal   forward_checking  # forward_checking(position, &puzzle_copy)
+  beq   $v0, $0, recursive_backtracking_skip
+
+  lw    $a0, 4($sp)     # solution
+  add   $a1, $sp, 24    # &puzzle_copy
+  jal   recursive_backtracking
+  beq   $v0, $0, recursive_backtracking_skip
+  j     recursive_backtracking_return_one # if (recursive_backtracking(solution, &puzzle_copy))
+recursive_backtracking_skip:
+  lw    $a0, 4($sp)     # solution
+  mul   $t0, $s0, 4
+  add   $t1, $a0, 4
+  add   $t1, $t1, $t0
+  sw    $0, 0($t1)      # solution->assignment[position] = 0
+  lw    $t0, 0($a0)
+  sub   $t0, $t0, 1
+  sw    $t0, 0($a0)     # solution->size -= 1
+recursive_backtracking_for_loop_continue:
+  add   $s1, $s1, 1     # val++
+  j     recursive_backtracking_for_loop
+recursive_backtracking_return_zero:
+  li    $v0, 0
+  j     recursive_backtracking_return
+recursive_backtracking_return_one:
+  li    $v0, 1
+recursive_backtracking_return:
+  lw    $ra, 0($sp)
+  lw    $a0, 4($sp)
+  lw    $a1, 8($sp)
+  lw    $s0, 12($sp)
+  lw    $s1, 16($sp)
+  lw    $s2, 20($sp)
+  add   $sp, $sp, 680
+  jr    $ra
+
+is_complete:
+  lw    $t0, 0($a0)       # solution->size
+  lw    $t1, 0($a1)       # puzzle->size
+  mul   $t1, $t1, $t1     # puzzle->size * puzzle->size
+  move	$v0, $0
+  seq   $v0, $t0, $t1
+  jr    $ra
+
+get_unassigned_position:
+  li    $v0, 0            # unassigned_pos = 0
+  lw    $t0, 0($a1)       # puzzle->size
+  mul  $t0, $t0, $t0     # puzzle->size * puzzle->size
+  add   $t1, $a0, 4       # &solution->assignment[0]
+get_unassigned_position_for_begin:
+  bge   $v0, $t0, get_unassigned_position_return  # if (unassigned_pos < puzzle->size * puzzle->size)
+  mul  $t2, $v0, 4
+  add   $t2, $t1, $t2     # &solution->assignment[unassigned_pos]
+  lw    $t2, 0($t2)       # solution->assignment[unassigned_pos]
+  beq   $t2, 0, get_unassigned_position_return  # if (solution->assignment[unassigned_pos] == 0)
+  add   $v0, $v0, 1       # unassigned_pos++
+  j   get_unassigned_position_for_begin
+get_unassigned_position_return:
+  jr    $ra
+
+
+.kdata				# interrupt handler data (separated just for readability)
+chunkIH:	.space 8	# space for two registers
+non_intrpt_str:	.asciiz "Non-interrupt exception\n"
+unhandled_str:	.asciiz "Unhandled interrupt type\n"
+
+
+.ktext 0x80000180
+interrupt_handler:
+.set noat
+	move	$k1, $at		# Save $at                               
+.set at
+	la	$k0, chunkIH
+	sw	$a0, 0($k0)		# Get some free registers                  
+	sw	$a1, 4($k0)		# by storing them to a global variable     
+
+	mfc0	$k0, $13		# Get Cause register                       
+	srl	$a0, $k0, 2                
+	and	$a0, $a0, 0xf		# ExcCode field                            
+	bne	$a0, 0, non_intrpt         
+
+interrupt_dispatch:			# Interrupt:                             
+	mfc0	$k0, $13		# Get Cause register, again                 
+	beq	$k0, 0, done		# handled all outstanding interrupts     
+
+	and	$a0, $k0, BONK_MASK	# is there a bonk interrupt?                
+	bne	$a0, 0, bonk_interrupt   
+
+	and	$a0, $k0, TIMER_MASK	# is there a timer interrupt?
+	bne	$a0, 0, timer_interrupt
+
+	# add dispatch for other interrupt types here.
+
+	and $a0, $k0, ON_FIRE_MASK
+	bne $a0, 0, on_fire_interrupt
+
+    and $a0, $k0, MAX_GROWTH_INT_MASK
+    bne $a0, 0, max_growth_interrupt
+
+    and $a0, $k0, REQUEST_PUZZLE_INT_MASK
+    bne $a0, 0, request_puzzle_interrupt
+
+	li	$v0, PRINT_STRING	# Unhandled interrupt types
+	la	$a0, unhandled_str
+	syscall 
+	j	done
+
+bonk_interrupt:
+	sw	$a1, BONK_ACK		# acknowledge interrupt
+	sw	$zero, VELOCITY		# ???
+
+	j	interrupt_dispatch	# see if other interrupts are waiting
+
+timer_interrupt:
+	sw	$a1, TIMER_ACK		# acknowledge interrupt
+
+	li	$t0, 90			# ???
+	sw	$t0, ANGLE		# ???
+	sw	$zero, ANGLE_CONTROL	# ???
+
+	lw	$v0, TIMER		# current time
+	add	$v0, $v0, 50000  
+	sw	$v0, TIMER		# request timer in 50000 cycles
+
+	j	interrupt_dispatch	# see if other interrupts are waiting
+
+on_fire_interrupt:
+	sw	$a1, ON_FIRE_ACK		# acknowledge interrupt
+
+	j   interrupt_dispatch
+
+max_growth_interrupt:
+    sw  $a1, MAX_GROWTH_ACK
+
+    j	interrupt_dispatch
+
+request_puzzle_interrupt:
+    sw  $a1, REQUEST_PUZZLE_ACK
+
+    j	interrupt_dispatch
+
+non_intrpt:				# was some non-interrupt
+	li	$v0, PRINT_STRING
+	la	$a0, non_intrpt_str
+	syscall				# print out an error message
+	# fall through to done
+
+done:
+	la	$k0, chunkIH
+	lw	$a0, 0($k0)		# Restore saved registers
+	lw	$a1, 4($k0)
+.set noat
+	move	$at, $k1		# Restore $at
+.set at 
+	eret
+
+
+
+
+
+
+
+
